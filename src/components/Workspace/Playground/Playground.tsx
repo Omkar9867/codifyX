0d1038d1eb080a515a7470242dc6a0998311a8f2
Omkar <omkarrajan495@gmail.com>
2024-11-19 23:51:21 +0530

    test cases  dummy

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -14,7 +14,7 @@ const Playground: React.FC<PlaygroundProps> = () => {
         <PreferanceNav />
 
         <Split
-          className='h-[calc(100vh-94px)]'
+          className="h-[calc(100vh-94px)]"
           direction="vertical"
           sizes={[60, 40]}
           minSize={60}
@@ -27,7 +27,60 @@ const Playground: React.FC<PlaygroundProps> = () => {
               style={{ fontSize: 16 }}
             />
           </div>
-          <div>Test Cases</div>
+          <div className="w-full px-5 overflow-auto">
+            {/* Test Case Heading */}
+            <div className="flex h-10 items-center space-x-6">
+              <div className="relative flex h-full flex-col justify-center cursor-pointer">
+                <div className="text-sm font-medium leading-5 text-white">
+                  TestCases
+                </div>
+                <hr className="absolute bottom-0 h-0.5 w-16 rounded-full border-none bg-white" />
+              </div>
+            </div>
+
+            <div className="flex">
+              {/* case 1 */}
+              <div className="mr-2 items-start mt-2 text-white">
+                <div className="flex flex-wrap items-center gap-y-4">
+                  <div
+                    className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3
+                        hover:bg-dark-fill-2 relative rounded-lg px-4 py1 cursor-pointer whitespace-nowrap"
+                  >
+                    Case 1
+                  </div>
+                </div>
+              </div>
+              {/* case 2 */}
+              <div className="mr-2 items-start mt-2 text-white">
+                <div className="flex flex-wrap items-center gap-y-4">
+                  <div
+                    className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3
+                        hover:bg-dark-fill-2 relative rounded-lg px-4 py1 cursor-pointer whitespace-nowrap"
+                  >
+                    Case 2
+                  </div>
+                </div>
+              </div>
+              {/* case 3 */}
+              <div className="mr-2 items-start mt-2 text-white">
+                <div className="flex flex-wrap items-center gap-y-4">
+                  <div
+                    className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3
+                        hover:bg-dark-fill-2 relative rounded-lg px-4 py1 cursor-pointer whitespace-nowrap"
+                  >
+                    Case 3
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="font-semibold">
+              <p className="text-sm font-medium mt-4 text-white">Input:</p>
+              <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+                nums: [2, 7, 11, 15], target: 9
+              </div>
+            </div>
+          </div>
         </Split>
       </div>
     </>
