adb5a439d89192778d0842f36462988cd40f423d
Omkar <omkarrajan495@gmail.com>
2024-11-19 23:35:03 +0530

    uiw-react-codemirror

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Split from "react-split";
 import ProblemDescription from "./ProblemDescription/ProblemDescription";
+import Playground from "./Playground/Playground";
 
 type WorkspaceProps = {};
 
 const Workspace: React.FC<WorkspaceProps> = () => {
   return (
     <>
-      <Split className="split">
+      <Split className="split" minSize={0}>
         <ProblemDescription />
-        <div>Code Editor is here</div>
+        <Playground/>
       </Split>
     </>
   );
