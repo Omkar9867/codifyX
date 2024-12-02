3f42b724585ff457f32d78f9503be0279ba4634d
Omkar <omkarrajan495@gmail.com>
2024-12-02 23:27:14 +0530

    adjusted the case color

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -47,14 +47,15 @@ const Playground: React.FC<PlaygroundProps> = ({ problem }) => {
               {/* cases*/}
               {problem.examples.map((example, idx) => (
                 <div
-                  className="mr-2 items-start mt-2 text-white"
+                  className="mr-2 items-start mt-2"
                   key={idx}
                   onClick={() => setActiveTestCaseId(idx)}
                 >
                   <div className="flex flex-wrap items-center gap-y-4">
                     <div
-                      className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3
-                        hover:bg-dark-fill-2 relative rounded-lg px-4 py1 cursor-pointer whitespace-nowrap"
+                      className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3
+                        hover:bg-dark-fill-2 relative rounded-lg px-4 py1 cursor-pointer whitespace-nowrap 
+                      ${activeTestCaseId === idx ? "text-white" : "text-gray-500"}`}
                     >
                       Case {idx + 1}
                     </div>
